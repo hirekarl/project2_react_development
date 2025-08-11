eabc9f536636b0c28b10e57f33179add696b1423
Karl Johnson <199382398+hirekarl@users.noreply.github.com>
2025-08-11 11:45:34 -0400

    Navbar has required elements. TODO: Fix layout and other styling.

--- url-shortener/src/components/NavBar.tsx
+++ url-shortener/src/components/NavBar.tsx
@@ -1,5 +1,29 @@
+import Container from "react-bootstrap/Container"
+import Nav from "react-bootstrap/Nav"
+import Navbar from "react-bootstrap/Navbar"
+import NavDropdown from "react-bootstrap/NavDropdown"
+
+import logo from "../assets/logo.svg"
+
 const NavBar = () => {
-  return <div>NavBar</div>
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">
+          <img src={logo} />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbar-nav" />
+        <Navbar.Collapse id="navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#">Features</Nav.Link>
+            <Nav.Link href="#">Pricing</Nav.Link>
+            <Nav.Link href="#">Login</Nav.Link>
+            <Nav.Link href="#">Sign Up</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  )
 }
 
 export default NavBar
